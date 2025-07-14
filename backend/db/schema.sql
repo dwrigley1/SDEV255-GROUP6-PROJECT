b147a40287d3d243d57ecbe433b77faaad044e05
Joshua Stine <jstine13@ivytech.edu>
2025-07-13 22:01:17 -0400

    added enrollment table to schema

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS courses;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS cart;
+DROP TABLE IF EXISTS enrollment;
 
 -- teachers and students
 CREATE TABLE users (
@@ -13,7 +14,7 @@ CREATE TABLE users (
 );
 
 -- created by teachers
-CREATE TABLe courses (
+CREATE TABLE courses (
   id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
   description TEXT NOT NULL,
@@ -30,6 +31,16 @@ CREATE TABLE cart (
   user_id INTEGER NOT NULL,
   course_id INTEGER NOT NULL,
   order_num TEXT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (course_id) REFERENCES courses(id)
+);
+
+---- Tracks which students(users) are enrolled in which courses
+
+CREATE TABLE enrollment (
+  user_id INTEGER NOT NULL,
+  course_id INTEGER NOT NULL,
+  PRIMARY KEY (user_id, course_id),
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (course_id) REFERENCES courses(id)
 );
