b36b1e9d2135dd24d08379bc3de0b767e9b99812
dwrigley1 <dwrigley1@ivytech.edu>
2025-07-21 19:43:45 -0400

    updated script.js

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 /* This is the script that will actually connect the login button to the backend */
 
-document.getElementById("loginForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  await fetch(`https://sdev255-group6-project.onrender.com/api/login/${email}/${password}`)
-    .then(res => res.json())
-    .then(data => {
-      console.log("Login success:", data);
-    })
-    .catch(err => {
-      console.error("Login failed:", err);
-    });
-});
-**/
+// new async function //
 
 document.getElementById("loginForm").addEventListener("submit", async function (e) {
   e.preventDefault();
