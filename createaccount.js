25cad2cdd7eb0826a1d5ab8e12ebafa386edb77b
dwrigley1 <dwrigley1@ivytech.edu>
2025-07-21 18:12:04 -0400

    added createaccount.js, updated html

---
+++ createaccount.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const user = {
+      first_name: document.querySelector("#fname").value,
+      last_name: document.querySelector("#lname").value,
+      email: document.querySelector("#email").value,
+      password: document.querySelector("#password").value,
+      role: document.querySelector("#role").value,
+    };
+      const response = await fetch(" URL HERE ", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        alert("Account successfully created!");
+      } else {
+        const err = await response.json();
+        alert("Unable to create account - " + err.message);
+      }
+    }
+  );
+});
