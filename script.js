8fd3b3bad4d6703301adf3d1c12e2884c908ac0d
dwrigley1 <dwrigley1@ivytech.edu>
2025-07-21 22:02:09 -0400

    another update

--- script.js
+++ script.js
@@ -2,15 +2,17 @@
 
 // new async function //
 
-document.getElementById("loginForm").addEventListener("submit", async function (e) {
-  e.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("loginForm").addEventListener("submit", async function (e) {
+    e.preventDefault();
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const response = await fetch(`https://sdev255-group6-project.onrender.com/api/login/${email}/${password}`);
-  if (!response.ok) {
-      throw new Error("Login failed with status " + response.status);
-    }
-    const data = await response.json();
-    console.log("Login success:", data);
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const response = await fetch(`https://sdev255-group6-project.onrender.com/api/login/${email}/${password}`);
+    if (!response.ok) {
+        throw new Error("Login failed with status " + response.status);
+      }
+      const data = await response.json();
+      console.log("Login success:", data);
   });
+});
