2e55379f2eea59e3d19ab9ea55307a2120a1505b
dwrigley1 <dwrigley1@ivytech.edu>
2025-07-26 09:21:44 -0400

    attempt 1 for acc creation

--- createaccount.js
+++ createaccount.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
       password: document.querySelector("#password").value,
       role: document.querySelector("#role").value,
     };
-      const response = await fetch("https://sdev255-group6-project.onrender.com/api/login", {
+      const response = await fetch(`https://sdev255-group6-project.onrender.com/api/login/${user_id}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
