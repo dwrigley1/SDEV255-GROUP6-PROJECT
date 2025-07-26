5be1d02a9de6da1183103581dcadf726ca35ffe5
dwrigley1 <dwrigley1@ivytech.edu>
2025-07-26 15:15:50 -0400

    acc creation

--- createaccount.js
+++ createaccount.js
@@ -3,15 +3,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
   form.addEventListener("submit", async function (e) {
     e.preventDefault();
+    
+    const fname = document.querySelector("#fname").value;
+    const lname = document.querySelector("#lname").value;
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    const role = document.querySelector("#role").value;
 
     const user = {
-      first_name: document.querySelector("#fname").value,
-      last_name: document.querySelector("#lname").value,
-      email: document.querySelector("#email").value,
-      password: document.querySelector("#password").value,
-      role: document.querySelector("#role").value,
+      first_name: fname,
+      last_name: lname,
+      email: email,
+      password: password,
+      role: role,
     };
-      const response = await fetch(`https://sdev255-group6-project.onrender.com/api/login/${fname}/${lname}/${email}/${password}/${role}`, {
+    
+      const response = await fetch(`https://sdev255-group6-project.onrender.com/api/login/`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -30,4 +37,15 @@ document.addEventListener("DOMContentLoaded", function () {
   );
 });
 
-// ${first_name}/${last_name}/${role}
+
+/**
+ * 
+ * ${fname}/${lname}/${email}/${password}/${role} 
+ const user = {
+      first_name: document.querySelector("#fname").value,
+      last_name: document.querySelector("#lname").value,
+      email: document.querySelector("#email").value,
+      password: document.querySelector("#password").value,
+      role: document.querySelector("#role").value,
+    };
+    **/
