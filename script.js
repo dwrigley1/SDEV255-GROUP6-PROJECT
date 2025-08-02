6a3f13869a76d00df034b07a059650705a4ad9f2
dwrigley1 <dwrigley1@ivytech.edu>
2025-08-02 17:06:04 -0400

    .

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ if (typeof CryptoJS === "undefined") {
   alert("CryptoJS failed to load."); // debugging
 }
 
-console.log(user.id)
 console.log(token)
 
 var role; // global var due to redeclaration issues
